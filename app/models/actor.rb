cd5addc5a5f6c94aba554e87a31f24f0273037d6
Agnes Park <agnespark824@gmail.com>
2020-03-07 22:37:58 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,8 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def list_roles
+    self.characters.map{| c | c.name + " - " + c.show}
+  end
 end
